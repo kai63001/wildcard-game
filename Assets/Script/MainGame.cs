729aa08cc5bdf7c866bd952b2e9fe96c754898b3
kai63001 <animehq5@gmail.com>
2021-11-29 22:23:19 +0700

    fix bug value not syn

--- Assets/Script/MainGame.cs
+++ Assets/Script/MainGame.cs
@@ -9,43 +9,25 @@ public class MainGame : MonoBehaviourPunCallbacks
     public Text romeoText;
     private int count = 0;
 
+
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
-        romeoText.text = "Get Start";
-    }
-
-    void Update()
-    {
-
-    }
-
-    public override void OnConnectedToMaster()
-    {
-        Debug.Log("OnConnectedToMaster() was called by PUN.");
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.IsVisible = false;
-        roomOptions.MaxPlayers = 4;
+        roomOptions.MaxPlayers = 2;
         PhotonNetwork.JoinOrCreateRoom("romeo", roomOptions, TypedLobby.Default);
     }
-
 
     public void draw()
     {
         // var output = JsonUtility.ToJson(PhotonNetwork.CurrentRoom, true);
         // Debug.Log(PhotonNetwork.CurrentRoom.Players.ElementAt(0));
-        if (photonView.IsMine)
-        {
-            counter(true);
-        }
-        else
-        {
-            base.photonView.RPC("counter", RpcTarget.All, true);
-            //photonView.RPC("AddFive", photonView.Controller);
-        }
+        base.photonView.RPC("counter", RpcTarget.All, true);
 
         Debug.Log(count);
     }
+
+
 
     [PunRPC]
     private void counter(bool test)
@@ -53,6 +35,8 @@ public class MainGame : MonoBehaviourPunCallbacks
         count++;
         romeoText.text = count.ToString();
         Debug.Log("Count");
+        //PhotonNetwork.RaiseEvent(count, count +1,RaiseEventOptions.Default, SendOptions.SendReliable);
     }
+
 
 }
