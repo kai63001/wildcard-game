6b761820604ec588ecfe99369f34a51b77d35a0d
kai63001 <animehq5@gmail.com>
2022-01-24 03:48:05 +0700

    add card contract

---
+++ contracts/Card.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract WileCard is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    uint256 public deposits = 0;
+    uint256 amount = 0;
+    address payable public Owner;
+    uint256 fee = 8;
+
+    constructor() ERC721("WileCard", "Wile") {
+        Owner = payable(msg.sender);
+    }
+
+    function mintNFT(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+
+    function totalSupply() public view returns (Counters.Counter memory) {
+        return _tokenIds;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == Owner, "Not owner");
+        _;
+    }
+
+    function deposit() public payable returns (uint256) {
+        require(msg.value == 1 * (10**fee), "MONEY U KONW");
+        return msg.value;
+    }
+
+    function changeFee(uint256 _fee) public returns (uint256) {
+        fee = _fee;
+        return fee;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function withdraw(uint256 _amount) public onlyOwner {
+        Owner.transfer(_amount);
+    }
+}
