405761715380ee19c77905140fa3625759a6349c
kai63001 <animehq5@gmail.com>
2022-01-24 04:08:10 +0700

    update web3

--- game/Assets/Web3Unity/Scripts/Prefabs/ERC721/ERC721URIExample.cs
+++ game/Assets/Web3Unity/Scripts/Prefabs/ERC721/ERC721URIExample.cs
@@ -6,10 +6,10 @@ public class ERC721URIExample : MonoBehaviour
 {
     async void Start()
     {
-        string chain = "polygon";
-        string network = "mainnet";
-        string contract = "0xbCCaa7ACb552A2c7eb27C7eb77c2CC99580735b9";
-        string tokenId = "965";
+        string chain = "ethereum";
+        string network = "rinkeby";
+        string contract = "0x8991664653f0665Cfa12765Da720756235cc1cbB";
+        string tokenId = "4";
 
         string uri = await ERC721.URI(chain, network, contract, tokenId);
         print(uri);
