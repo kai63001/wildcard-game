09e0806f65dd1df71c83ea8e7cdd6ced24f16884
kai63001 <animehq5@gmail.com>
2022-01-25 20:16:48 +0700

    add fonts

--- dapp/components/Navbar.tsx
+++ dapp/components/Navbar.tsx
@@ -1,7 +1,12 @@
 const Navbar = () => {
-    return (
-        <nav>asdasd</nav>
-    )
-}
+  return (
+    <nav className="bg-black text-white">
+      <div className="flex justify-between max-w-screen-xl mx-auto px-2 xs:px-0 py-4">
+        <div className="test">asdasd</div>
+        <div className="test">asdasd</div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
