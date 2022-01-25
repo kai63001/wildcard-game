bf55a254e7228d01e12223cd3b9ae6ab154af6f6
kai63001 <animehq5@gmail.com>
2022-01-25 20:42:40 +0700

    update login with metamask

--- dapp/components/Navbar.tsx
+++ dapp/components/Navbar.tsx
@@ -1,10 +1,12 @@
+import {init} from "@/lib/Web3Client"
+
 const Navbar = () => {
   return (
     <nav className="bg-black text-white">
       <div className="flex justify-between max-w-screen-xl mx-auto px-2 xs:px-0 py-3">
         <div className="text-2xl mt-1">WILE GAME</div>
         <div className="test">
-          <button className="text-black bg-white px-3 py-2 rounded-md">
+          <button onClick={init} className="text-black bg-white px-3 py-2 rounded-md">
             Login with Metamask
           </button>
         </div>
