9f1cfb164eff2de61dade71424d5083e23bf49ac
kai63001 <animehq5@gmail.com>
2022-01-25 21:39:32 +0700

    login with metamask

--- dapp/components/Navbar.tsx
+++ dapp/components/Navbar.tsx
@@ -1,14 +1,45 @@
-import {init} from "@/lib/Web3Client"
+import { init, isAuth } from "@/lib/Web3Client";
+import { useEffect, useState } from "react";
+// import { isAuth } from "@/lib/JWT";
+// import { Cookies } from "react-cookie";
+// const cookies = new Cookies();
 
 const Navbar = () => {
+  const [auth, setAuth] = useState("");
+  useEffect(() => {
+    getAuth();
+  });
+
+  const getAuth = async () => {
+    try {
+      init().then( async(data)=>{
+        setAuth(await isAuth());
+      })
+      
+    } catch (error) {
+      console.log("login first");
+    }
+  };
+
   return (
     <nav className="bg-black text-white">
       <div className="flex justify-between max-w-screen-xl mx-auto px-2 xs:px-0 py-3">
         <div className="text-2xl mt-1">WILE GAME</div>
         <div className="test">
-          <button onClick={init} className="text-black bg-white px-3 py-2 rounded-md">
-            Login with Metamask
-          </button>
+          {auth ? (
+            <button
+              className="text-black bg-white px-3 py-2 rounded-md"
+            >
+              {auth}
+            </button>
+          ) : (
+            <button
+              onClick={init}
+              className="text-black bg-white px-3 py-2 rounded-md"
+            >
+              Login with Metamask
+            </button>
+          )}
         </div>
       </div>
     </nav>
--- dapp/lib/Web3Client.ts
+++ dapp/lib/Web3Client.ts
@@ -1,11 +1,11 @@
 import { ethers } from "ethers";
-import {sign} from "@/lib/JWT"
+// import { sign } from "@/lib/JWT";
 const conTractAddress = "0xE1B8557dEc3984eFA4Fa2078FFEAC8df50C0D27B";
 const abi = `[ { "inputs": [ { "internalType": "string", "name": "name_", "type": "string" }, { "internalType": "string", "name": "symbol_", "type": "string" } ], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "approved", "type": "address" }, { "indexed": true, "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "operator", "type": "address" }, { "indexed": false, "internalType": "bool", "name": "approved", "type": "bool" } ], "name": "ApprovalForAll", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": true, "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "Transfer", "type": "event" }, { "inputs": [ { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "approve", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "owner", "type": "address" } ], "name": "balanceOf", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "getApproved", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "operator", "type": "address" } ], "name": "isApprovedForAll", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "name", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "ownerOf", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "safeTransferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" }, { "internalType": "bytes", "name": "_data", "type": "bytes" } ], "name": "safeTransferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "operator", "type": "address" }, { "internalType": "bool", "name": "approved", "type": "bool" } ], "name": "setApprovalForAll", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "bytes4", "name": "interfaceId", "type": "bytes4" } ], "name": "supportsInterface", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "tokenURI", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "transferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" } ]`;
 
 let myAddress = "";
 let contract;
-let provider;
+let provider: ethers.providers.Web3Provider;
 export const init = async () => {
   //@ts-ignore
   provider = new ethers.providers.Web3Provider(web3.currentProvider, "rinkeby");
@@ -14,10 +14,22 @@ export const init = async () => {
   const signer = provider.getSigner();
   //   console.log(signer);
   contract = new ethers.Contract(conTractAddress, abi, signer);
+  console.log("init");
   //   console.log(contract);
   //   const balance = await contract.balanceOf(await signer.getAddress());
   //   console.log(balance.toString());
-  const getName = await contract.name();
-  console.log(getName.toString());
-  sign(await signer.getAddress());
+  //   const getName = await contract.name();
+  //   console.log(getName.toString());
+  //   const address = await signer?.getAddress();
+  //   console.log(address)
+  //   sign(await signer.getAddress());
+  //   myAddress = await signer.getAddress();
+  return true
+};
+
+export const isAuth = async () => {
+  let address = "";
+  const signer = provider?.getSigner();
+  address = await signer?.getAddress();
+  return address;
 };
