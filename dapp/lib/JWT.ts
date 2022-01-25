efe9fc50716312a80974c6cb69617d84013bfe4f
kai63001 <animehq5@gmail.com>
2022-01-25 21:09:42 +0700

    jwt sign token cookie

--- dapp/lib/JWT.ts
+++ dapp/lib/JWT.ts
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 const privateKey = "justfortest";
+import Cookies from 'js-cookie'
 
 export const sign = (adress: string) => {
   console.log(adress);
@@ -7,8 +8,21 @@ export const sign = (adress: string) => {
     { adress: adress },
     privateKey,
     { algorithm: "HS256" },
-    function (err, token) {
+    function (err, token:any) {
       console.log(token);
+      Cookies.set('token', token)
     }
   );
+};
+
+
+export const isAuth =  (token:string) => {
+    if(token) {
+        try {
+            return jwt.verify(token, privateKey);
+        } catch (error) {
+            return null
+        }
+    }
+    return null
 };
