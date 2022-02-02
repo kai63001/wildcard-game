3c1c9b98f07cf9b8f2b54048c00028d391ac5ed4
kai63001 <animehq5@gmail.com>
2022-02-03 01:45:39 +0700

    create deploy.json to get address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,17 @@
+var fs = require('fs');
+
 async function main() {
     const MyContact = await ethers.getContractFactory("WileCard");
     const myContact = await MyContact.deploy();
   
+    var obj = {
+        "address": myContact.address
+    }
+    await fs.writeFileSync('deploy.json', JSON.stringify(obj), function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
     console.log("My Contract deployed to : ", myContact.address);
-    
   }
   
   main().then(()=>process.exit(0)).catch(error => {
