1c223f2becf2980a08121bc95198274db8fb500e
kai63001 <animehq5@gmail.com>
2022-02-09 21:02:41 +0700

    add random card ether.js

--- dapp/lib/Web3Client.ts
+++ dapp/lib/Web3Client.ts
@@ -1,13 +1,12 @@
 import { BigNumber, ethers } from "ethers";
 import data from "../../artifacts/contracts/Card.sol/WileCard.json"; // get data abi
-import addressData from "../../deploy.json"
-
+import addressData from "../../deploy.json";
 
 const conTractAddress = addressData.address; // * nft contractaddress
 // * abi of nft address
 // const abi = "[ { \"inputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"constructor\" }, { \"anonymous\": false, \"inputs\": [ { \"indexed\": true, \"internalType\": \"address\", \"name\": \"owner\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"address\", \"name\": \"approved\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"Approval\", \"type\": \"event\" }, { \"anonymous\": false, \"inputs\": [ { \"indexed\": true, \"internalType\": \"address\", \"name\": \"owner\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"address\", \"name\": \"operator\", \"type\": \"address\" }, { \"indexed\": false, \"internalType\": \"bool\", \"name\": \"approved\", \"type\": \"bool\" } ], \"name\": \"ApprovalForAll\", \"type\": \"event\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"approve\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"uint256\", \"name\": \"_fee\", \"type\": \"uint256\" } ], \"name\": \"changeFee\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"deposit\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"payable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"string\", \"name\": \"tokenURI\", \"type\": \"string\" } ], \"name\": \"mintNFT\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"safeTransferFrom\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" }, { \"internalType\": \"bytes\", \"name\": \"_data\", \"type\": \"bytes\" } ], \"name\": \"safeTransferFrom\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"operator\", \"type\": \"address\" }, { \"internalType\": \"bool\", \"name\": \"approved\", \"type\": \"bool\" } ], \"name\": \"setApprovalForAll\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"anonymous\": false, \"inputs\": [ { \"indexed\": true, \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"indexed\": true, \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"Transfer\", \"type\": \"event\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"transferFrom\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"from\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"to\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"transferNFT\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"uint256\", \"name\": \"_amount\", \"type\": \"uint256\" } ], \"name\": \"withdraw\", \"outputs\": [], \"stateMutability\": \"nonpayable\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"owner\", \"type\": \"address\" } ], \"name\": \"balanceOf\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"deposits\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"getApproved\", \"outputs\": [ { \"internalType\": \"address\", \"name\": \"\", \"type\": \"address\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"getBalance\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"getUserToken\", \"outputs\": [ { \"internalType\": \"uint256[]\", \"name\": \"\", \"type\": \"uint256[]\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"sender\", \"type\": \"address\" } ], \"name\": \"getUserTokenWithAddress\", \"outputs\": [ { \"internalType\": \"uint256[]\", \"name\": \"\", \"type\": \"uint256[]\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"owner\", \"type\": \"address\" }, { \"internalType\": \"address\", \"name\": \"operator\", \"type\": \"address\" } ], \"name\": \"isApprovedForAll\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"name\", \"outputs\": [ { \"internalType\": \"string\", \"name\": \"\", \"type\": \"string\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"Owner\", \"outputs\": [ { \"internalType\": \"address payable\", \"name\": \"\", \"type\": \"address\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"ownerOf\", \"outputs\": [ { \"internalType\": \"address\", \"name\": \"\", \"type\": \"address\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"bytes4\", \"name\": \"interfaceId\", \"type\": \"bytes4\" } ], \"name\": \"supportsInterface\", \"outputs\": [ { \"internalType\": \"bool\", \"name\": \"\", \"type\": \"bool\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"symbol\", \"outputs\": [ { \"internalType\": \"string\", \"name\": \"\", \"type\": \"string\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"uint256\", \"name\": \"tokenId\", \"type\": \"uint256\" } ], \"name\": \"tokenURI\", \"outputs\": [ { \"internalType\": \"string\", \"name\": \"\", \"type\": \"string\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [], \"name\": \"totalSupply\", \"outputs\": [ { \"components\": [ { \"internalType\": \"uint256\", \"name\": \"_value\", \"type\": \"uint256\" } ], \"internalType\": \"struct Counters.Counter\", \"name\": \"\", \"type\": \"tuple\" } ], \"stateMutability\": \"view\", \"type\": \"function\" }, { \"inputs\": [ { \"internalType\": \"address\", \"name\": \"\", \"type\": \"address\" }, { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"name\": \"userOwnedTokens\", \"outputs\": [ { \"internalType\": \"uint256\", \"name\": \"\", \"type\": \"uint256\" } ], \"stateMutability\": \"view\", \"type\": \"function\" } ]";
 const abi = data.abi;
- 
+
 // let myAddress = "";
 let contract: ethers.Contract;
 let provider: ethers.providers.Web3Provider;
@@ -21,10 +20,10 @@ export const init = async () => {
   provider = new ethers.providers.Web3Provider(web3.currentProvider, "ropsten");
   await provider.send("eth_requestAccounts", []);
   const signer = provider.getSigner();
-  console.log(signer)
+  console.log(signer);
   contract = new ethers.Contract(conTractAddress, abi, signer);
   console.log("init");
-  console.log(contract)
+  console.log(contract);
   return true;
 };
 
@@ -50,35 +49,36 @@ export const getUserToken = () => {
       // ? promise to get tokeURI from array token id like [1,2,3]
       const promises = await data.map(async (data: unknown) => {
         const numFruit = new Promise((resolve, reject) => {
-          resolve(contract.tokenURI(data))
+          resolve(contract.tokenURI(data));
         });
-        return numFruit
-      })
-      const numFruits = await Promise.all(promises)
+        return numFruit;
+      });
+      const numFruits = await Promise.all(promises);
       res(await numFruits);
     });
   });
 };
 
-
-export const getUserTokenViaAddress = (contract:any) => {
+export const getUserTokenViaAddress = (contract: any) => {
   // contract.tokenURI
   return new Promise(function (res, rej) {
-    contract.getUserTokenWithAddress('0xF58F1e730fd6bDd0c239E1D83eaB9d87132eF723').then(async function (transaction: any) {
-      let data = await transaction.filter(
-        (data: any) => data.toNumber() != 0 && data.toNumber()
-      );
-      data = await data.map((data: any) => data.toNumber());
-      // ? promise to get tokeURI from array token id like [1,2,3]
-      const promises = await data.map(async (data: unknown) => {
-        const numFruit = new Promise((resolve, reject) => {
-          resolve(contract.tokenURI(data))
+    contract
+      .getUserTokenWithAddress("0xF58F1e730fd6bDd0c239E1D83eaB9d87132eF723")
+      .then(async function (transaction: any) {
+        let data = await transaction.filter(
+          (data: any) => data.toNumber() != 0 && data.toNumber()
+        );
+        data = await data.map((data: any) => data.toNumber());
+        // ? promise to get tokeURI from array token id like [1,2,3]
+        const promises = await data.map(async (data: unknown) => {
+          const numFruit = new Promise((resolve, reject) => {
+            resolve(contract.tokenURI(data));
+          });
+          return numFruit;
         });
-        return numFruit
-      })
-      const numFruits = await Promise.all(promises)
-      res(await numFruits);
-    });
+        const numFruits = await Promise.all(promises);
+        res(await numFruits);
+      });
   });
 };
 
@@ -91,4 +91,19 @@ export const isAuth = async () => {
   const signer = provider?.getSigner();
   address = await signer?.getAddress();
   return address;
+};
+
+/**
+ * * random card nft
+ * @returns
+ */
+export const randomNFT = async () => {
+  console.log("randomNFT");
+  try {
+    contract.randomNFT().then(async function (transaction: any) {
+      console.log(transaction);
+    });
+  } catch (error) {
+    console.log("out of nft");
+  }
 };
--- dapp/pages/marketplace/index.tsx
+++ dapp/pages/marketplace/index.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/Layout";
 import Link from "next/link";
+import { randomNFT } from "@/lib/Web3Client";
 
 const Marketplace = () => {
   return (
@@ -7,20 +8,105 @@ const Marketplace = () => {
       <div className="grid grid-cols-4 gap-4">
         <div className="...">
           <h2 className="text-2xl mb-3">Management</h2>
-          <button className="text-center w-full py-2 bg-red-400 rounded-md mb-2">
+          <button
+            onClick={() => randomNFT()}
+            className="text-center w-full py-2 border-2 border-red-400 text-red-400 hover:bg-red-400 hover:text-white duration-150 rounded-md mb-2"
+          >
             Random Card
           </button>
           <Link href="/marketplace/sell">
-          <a className="text-center w-full py-2 bg-blue-400 rounded-md block">
-            Sell Card
-          </a>
+            <a className="text-center w-full py-2 bg-blue-400 rounded-md block">
+              Sell Card
+            </a>
           </Link>
+          <h2 className="text-2xl mt-3">Rarity</h2>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input  h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+              type="checkbox"
+              id="inlineCheckbox1"
+              value="option1"
+            />
+            <label
+              className="form-check-label inline-block "
+              htmlFor="inlineCheckbox1"
+            >
+              s
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input  h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+              type="checkbox"
+              id="inlineCheckbox2"
+              value="option2"
+            />
+            <label
+              className="form-check-label inline-block "
+              htmlFor="inlineCheckbox2"
+            >
+              sr
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input   h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 
+                focus:outline-none duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+              type="checkbox"
+              id="inlineCheckbox2"
+              value="option2"
+            />
+            <label
+              className="form-check-label inline-block "
+              htmlFor="inlineCheckbox2"
+            >
+              ssr
+            </label>
+          </div>
+          <div className="relative pt-1">
+            <label htmlFor="customRange" className="form-label ">
+              Price <br />{" "}
+            </label>
+            <input
+              type="range"
+              className="form-range w-65% h-1 focus:outline-none focus:ring-1 focus:shadow-none"
+              min="0"
+              max="500"
+              step="0.5"
+              defaultValue={0}
+              id="customRange"
+            />
+            <div></div>
+
+            <div className="relative pt-1">
+              <label className="form-label  ">
+                Mana <br />{" "}
+              </label>
+              <input
+                type="range"
+                className="
+                    form-range
+                    w-65%
+                    h-1
+                    value
+                    focus:outline-none focus:ring-1 focus:shadow-none
+    "
+                min="0"
+                max="500"
+                step="0.5"
+                defaultValue={0}
+              />
+            </div>
+          </div>
         </div>
         <div className="col-span-3">
           <h2 className="text-2xl mb-3">Cards</h2>
           <div className="grid grid-cols-4 gap-4">
             <div className="bg-white overflow-hidden rounded-md text-gray-900">
-              <img src="https://marscatsvoyage.com/uploads/tmp/bc/a6/BcA6N6QLd9.png" alt="" />
+              <img
+                src="https://marscatsvoyage.com/uploads/tmp/bc/a6/BcA6N6QLd9.png"
+                alt=""
+              />
               <div className="my-2 p-2">
                 <h3>Cool Cat #123</h3>
                 <p className="text-smm text-gray-600">0.001 ETH</p>
