5034d90b26c0ea6136d8a8ced1faa00ff6592057
kai63001 <animehq5@gmail.com>
2022-02-09 22:03:31 +0700

    update Image component

--- dapp/components/Card/MyCard.tsx
+++ dapp/components/Card/MyCard.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image"
+
 const Card = (props: any) => {
   return (
     <div>
       <div className="bg-white overflow-hidden rounded-md text-gray-900">
-        <img src={props.item.image} alt="" />
+        <div className="bg-gray-300 h-60 overflow-hidden relative">
+        <Image
+            className="rounded-tr-md rounded-tl-md max-h-56"
+            src={props.item.image}
+            alt={props.item.name}
+            title={props.item.name}
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
         <div className="my-2 p-2">
           <h3>{props.item.name} #123</h3>
           <p className="text-smm text-gray-600">0.001 ETH</p>
--- dapp/next.config.js
+++ dapp/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['ipfs.infura.io', 'ipfs.io'],
+  },
 }
 
 module.exports = nextConfig
