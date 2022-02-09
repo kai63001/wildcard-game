9193574e08e0e7de7a20e6940f7f84dbd5648534
kai63001 <animehq5@gmail.com>
2022-02-09 22:18:30 +0700

    add my page nft by token id

--- dapp/components/Card/MyCard.tsx
+++ dapp/components/Card/MyCard.tsx
@@ -1,25 +1,29 @@
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
 
 const Card = (props: any) => {
   return (
-    <div>
-      <div className="bg-white overflow-hidden rounded-md text-gray-900">
-        <div className="bg-gray-300 h-60 overflow-hidden relative">
-        <Image
-            className="rounded-tr-md rounded-tl-md max-h-56"
-            src={props.item.image}
-            alt={props.item.name}
-            title={props.item.name}
-            layout="fill"
-            objectFit="cover"
-          />
+    <Link href={`/marketplace/my/${props.item.tokenId}`}>
+      <a>
+        <div className="bg-white overflow-hidden rounded-md text-gray-900">
+          <div className="bg-gray-300 h-60 overflow-hidden relative">
+            <Image
+              className="rounded-tr-md rounded-tl-md max-h-56"
+              src={props.item.image}
+              alt={props.item.name}
+              title={props.item.name}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="my-2 p-2">
+            <h3>
+              {props.item.name} #{props.item.tokenId}
+            </h3>
+          </div>
         </div>
-        <div className="my-2 p-2">
-          <h3>{props.item.name} #{props.item.tokenId}</h3>
-          <p className="text-smm text-gray-600">0.001 ETH</p>
-        </div>
-      </div>
-    </div>
+      </a>
+    </Link>
   );
 };
 
---
+++ dapp/pages/marketplace/my/[tokenId].tsx
@@ -0,0 +1,13 @@
+import Layout from "@/components/Layout"
+
+const MyTokenIdNft = () => {
+    return (
+        <Layout>
+            <div>
+                hello
+            </div>
+        </Layout>
+    )
+}
+
+export default MyTokenIdNft;
