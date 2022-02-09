29d20ffd5416f274aef4d2d91287d88b187ff2f1
kai63001 <animehq5@gmail.com>
2022-02-09 22:57:07 +0700

    update mint to minNFT([array])

--- dapp/lib/Web3Client.ts
+++ dapp/lib/Web3Client.ts
@@ -29,7 +29,7 @@ export const init = async () => {
 
 export const mint = async (data: string) => {
   console.log(contract);
-  contract.mintNFT(data).then(function (transaction: any) {
+  contract.mintNFT([data]).then(function (transaction: any) {
     console.log(transaction);
   });
 };
