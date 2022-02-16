ba2b367145f3a3eed32bf3a018c081d69361868a
kai63001 <animehq5@gmail.com>
2022-02-16 18:12:40 +0700

    add fucntion check event

--- dapp/components/Card/TradingHistory.tsx
+++ dapp/components/Card/TradingHistory.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import dayjs from "@/lib/dayjs"
+import dayjs from "@/lib/dayjs";
+import React from "react";
+import deploy from "../../../deploy.json";
 
 const TradingHistory = (props: any) => {
   const [history, setHistory] = useState([]);
@@ -10,11 +12,34 @@ const TradingHistory = (props: any) => {
 
   const getHistory = async () => {
     const data = await fetch(
-      `https://api.covalenthq.com/v1/97/tokens/0x50B5BdBeaCeDe6AEB22d1C2b4A8A37a1A1FD71E9/nft_transactions/${props.tokenId}/?&key=ckey_08290dd811f54239a7d5ac20831`
+      `https://api.covalenthq.com/v1/97/tokens/${deploy.address}/nft_transactions/${props.tokenId}/?&key=ckey_08290dd811f54239a7d5ac20831`
     );
     const res = await data.json();
     setHistory(res.data.items[0].nft_transactions);
     console.log(res.data.items[0].nft_transactions);
+  };
+
+  const getEventCalue = (event: any) => {
+    if (
+      event.log_events[0].decoded.params[0].value ==
+      "0x0000000000000000000000000000000000000000"
+    ) {
+      return "Mint";
+    }
+    if (
+      event.log_events[0].decoded.params[1].value.toUpperCase() ==
+      deploy.address.toUpperCase()
+    ) {
+      return "List";
+    }
+    if (
+      event.log_events[0].decoded.params[0].value.toUpperCase() ==
+        deploy.address.toUpperCase() &&
+      event.value != 0
+    ) {
+      return "Sell";
+    }
+    return "Tranfer";
   };
 
   return (
@@ -28,13 +53,22 @@ const TradingHistory = (props: any) => {
         <div className="">Date</div>
         {history.map((item: any, index: any) => {
           return (
-            <>
-              <div className="">Event</div>
-              <div className="">{item.value / (10 ** 18)} BNB</div>
-              <div className="text-ellipsis overflow-hidden">{item.log_events[0].decoded.params[0].value}</div>
-              <div className="text-ellipsis overflow-hidden">{item.log_events[0].decoded.params[1].value}</div>
-              <div className="">{dayjs().to(Date.parse(item.block_signed_at))}</div>
-            </>
+            <React.Fragment key={index}>
+              <div className="">{getEventCalue(item)}</div>
+              <div className="">{item.value / 10 ** 18} BNB</div>
+              <div className="text-ellipsis overflow-hidden">
+                {item.log_events[0].decoded.params[0].value}
+              </div>
+              <div className="text-ellipsis overflow-hidden">
+                {item.log_events[0].decoded.params[1].value}
+              </div>
+              <div className="">
+                {
+                  //@ts-ignore
+                  dayjs().to(Date.parse(item.block_signed_at))
+                }
+              </div>
+            </React.Fragment>
           );
         })}
       </div>
