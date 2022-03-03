98a3dc873835d90f255e9178736a2d51780c2c83
Romeo <supanat0245@gmail.com>
2022-03-03 16:35:58 +0700

    update blackjack cal

--- godot/Script/Enemy/Health.gd
+++ godot/Script/Enemy/Health.gd
@@ -3,6 +3,8 @@ extends Control
 var HP = 50;
 onready var HPProgress = get_node("HealthProgress")
 onready var StartGame = get_parent().get_parent().get_node(".")
+onready var HPMy = get_parent().get_parent().get_node("MyUI/Health")
+onready var HPMyProgress = get_parent().get_parent().get_node("MyUI/Health/HealthProgress")
 
 signal changeMyCountBar(value)
 signal changeEnemyCountBar(value)
@@ -23,6 +25,9 @@ func _on_StartGame_enemyCountChangeSignal(count):
 		emit_signal("changeMyCountBar",0)
 		emit_signal("changeEnemyCountBar",0)
 	elif(count == 12):
+		var calHP = 12 - StartGame.myCount
+		HPMy.HP -= calHP
+		HPMyProgress.value = HPMy.HP
 		StartGame.myCount = 0
 		StartGame.enemyCount = 0
 		emit_signal("changeMyCountBar",0)
--- godot/Script/My/Health.gd
+++ godot/Script/My/Health.gd
@@ -3,6 +3,8 @@ extends Control
 var HP = 50;
 onready var HPProgress = get_node("HealthProgress")
 onready var StartGame = get_parent().get_parent().get_node(".")
+onready var HPENEMYProgress = get_parent().get_parent().get_node("EnemyUI/Health/HealthProgress")
+onready var HPENEMY = get_parent().get_parent().get_node("EnemyUI/Health")
 
 signal changeMyCountBar(value)
 signal changeEnemyCountBar(value)
@@ -22,6 +24,9 @@ func _on_DrawCard_myCountValue(count):
 		emit_signal("changeMyCountBar",0)
 		emit_signal("changeEnemyCountBar",0)
 	elif(count == 12):
+		var calHP = 12 - StartGame.enemyCount
+		HPENEMY.HP -= calHP
+		HPENEMYProgress.value = HPENEMY.HP
 		StartGame.myCount = 0
 		StartGame.enemyCount = 0
 		emit_signal("changeMyCountBar",0)
