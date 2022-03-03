6766ca9fc88777cee5146c598049b81e9306abfa
Romeo <supanat0245@gmail.com>
2022-03-03 22:17:26 +0700

    change carddeck and add hover pointer cursor

--- godot/Screen/DrawCard.gd
+++ godot/Screen/DrawCard.gd
@@ -6,7 +6,7 @@ onready var StartGame = get_parent().get_parent().get_node(".")
 onready var MainGame = get_parent().get_parent().get_parent().get_node(".")
 onready var ServerConnect = get_node("/root/Main/ServerConnect")
 
-onready var CardBack = load("res://Assets/CardDeck.png")
+onready var CardBack = load("res://Assets/CardDeck2.png")
 onready var CardBackBorder = load("res://Assets/CardDeckBorder.png")
 
 func _on_DrawCard_pressed():
