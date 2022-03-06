498e190dc4ca64b8cf21d7c2913a4682192dff8d
Romeo <supanat0245@gmail.com>
2022-03-07 03:32:04 +0700

    change black to white background

--- dapp/components/Card/MyCard.tsx
+++ dapp/components/Card/MyCard.tsx
@@ -5,23 +5,24 @@ const Card = (props: any) => {
   return (
     <Link href={`/marketplace/my?tokenId=${props.item.tokenId}`} as={`/marketplace/my/${props.item.tokenId}`}>
       <a>
-        <div className="bg-white overflow-hidden rounded-md text-gray-900">
-          <div className="bg-gray-300 h-72 overflow-hidden relative">
+        <div className="bg-white overflow-hidden text-gray-900">
+          <div className="bg-gray-300 h-80 overflow-hidden relative">
             <Image
               className="rounded-tr-md rounded-tl-md max-h-56"
               src={props.item.image}
               alt={props.item.name}
               title={props.item.name}
               layout="fill"
+              quality={100}
               blurDataURL="data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAb/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWEREiMxUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
               placeholder="blur"
             />
           </div>
-          <div className="my-2 p-2">
+          {/* <div className="my-2 p-2">
             <h3>
               {props.item.name} #{props.item.tokenId}
             </h3>
-          </div>
+          </div> */}
         </div>
       </a>
     </Link>
