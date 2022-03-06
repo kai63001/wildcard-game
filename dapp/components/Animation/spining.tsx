e9d6c8a48975c008d29c1d55093eb32557bb1b4c
Romeo <supanat0245@gmail.com>
2022-03-07 06:21:27 +0700

    change spining color

--- dapp/components/Animation/spining.tsx
+++ dapp/components/Animation/spining.tsx
@@ -1,7 +1,7 @@
 const Spining = () => {
   return (
     <svg
-      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white inline-block"
+      className="animate-spin -ml-1 mr-3 h-5 w-5 text-red-600 inline-block"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
